1bc314d2a09f9b3fc7550c853eae6de32eeea7ae
Shinpoo <0xshinpoo@gmail.com>
2022-08-11 15:22:07 +0200

    remove auth

--- src/StakedKnife.sol
+++ src/StakedKnife.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "./Authorizable.sol";
 import "./MPLegacyToken.sol";
 
 
